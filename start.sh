fccd49eddd27be708cf5fe6d7ba7b2cb32a52c19
not-alone <not-alone@yandex.ru>
2019-05-05 21:39:01 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,18 +2,18 @@
 umask 0000
 if [ ! -f /OneDriveConf/refresh_token ]
 then
-  echo "OneDrive Authentication" >> /OneDriveConf/log.txt 2>&1
-  echo $ONEDRIVE_AUTH | /usr/local/bin/onedrive --confdir /OneDriveConf >> /OneDriveConf/log.txt 2>&1
+  echo "OneDrive Authentication"
+  /usr/local/bin/onedrive --confdir /OneDriveConf
 fi
 
 if [ ! -f /OneDriveConf/config ]
 then
-  echo "Creating Config File" >> /OneDriveConf/log.txt 2>&1
-  cp -f /home/nobody/.config/onedrive/config /OneDriveConf/config
+  echo "Creating Config File"
+  cp -f /home/nobody/config /OneDriveConf/config
 fi
 
 if [ -f /OneDriveConf/refresh_token ]
 then
-  echo "Running OneDrive" >> /OneDriveConf/log.txt 2>&1
-  /usr/local/bin/onedrive $ONEDRIVE_COMMANDS --confdir /OneDriveConf --syncdir /OneDriveData >> /OneDriveConf/log.txt 2>&1
+  echo "Running OneDrive"
+  /usr/local/bin/onedrive $ONEDRIVE_COMMANDS --monitor --verbose --confdir /OneDriveConf --syncdir /OneDriveData
 fi
