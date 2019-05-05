8b02c9205d398d703967d607414b1c10d6f2411b
not-alone <not-alone@yandex.ru>
2019-05-05 22:12:10 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,12 +6,6 @@ then
   /usr/local/bin/onedrive --confdir /OneDriveConf
 fi
 
-if [ ! -f /OneDriveConf/config ]
-then
-  echo "Creating Config File"
-  cp -f /home/nobody/config /OneDriveConf/config
-fi
-
 if [ -f /OneDriveConf/refresh_token ]
 then
   echo "Running OneDrive"
